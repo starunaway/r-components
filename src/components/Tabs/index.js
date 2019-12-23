d9fed4c88e03a7ad5d1788fe68746461b5f869fb
keeping <starunaway@gmail.com>
2019-12-23 17:17:52 +0800

    feat: use ctrl key to multi select

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,20 +1,16 @@
 /**
  * Created by starunaway on 2019/7/18.
  */
-import React from "react";
-import "./style.less";
+import React from 'react';
+import './style.less';
 
 class Tabs extends React.PureComponent {
   constructor(props) {
     super(props);
-    let value = Array.isArray(props.value)
-      ? props.value
-      : props.value
-      ? [props.value]
-      : [];
+    let value = Array.isArray(props.value) ? props.value : props.value ? [props.value] : [];
     this.Determine(value, props.disabledItem);
     this.isMulti(value);
-    value = value.length === 0 && props.showAll ? ["ALL"] : value;
+    value = value.length === 0 && props.showAll ? ['ALL'] : value;
     this.state = {
       value
     };
@@ -29,9 +25,9 @@ class Tabs extends React.PureComponent {
   }
 
   isMulti = (value) => {
-    const { multi } = this.props;
+    const {multi} = this.props;
     if (!multi && value.length > 1) {
-      throw new Error("未设置multi,但设置了多个选中值");
+      throw new Error('未设置multi,但设置了多个选中值');
     }
   };
 
@@ -41,20 +37,20 @@ class Tabs extends React.PureComponent {
         disabledItem.forEach((dI) => {
           value.forEach((v) => {
             if (dI === v) {
-              throw new Error(dI + " is disabled");
+              throw new Error(dI + ' is disabled');
             }
           });
         });
       } else {
         if (value.indexOf(disabledItem) > -1) {
-          throw new Error(disabledItem + " is disabled");
+          throw new Error(disabledItem + ' is disabled');
         }
       }
     }
   };
 
   DetermineDisabled = (item) => {
-    const { disabledItem } = this.props;
+    const {disabledItem} = this.props;
     if (disabledItem instanceof Array) {
       return disabledItem.indexOf(item.key) > -1;
     } else {
@@ -63,63 +59,56 @@ class Tabs extends React.PureComponent {
   };
 
   DetermineChecked = (item) => {
-    const { value } = this.state;
+    const {value} = this.state;
     return (value || []).indexOf(item.key) > -1;
   };
 
   handleClick = (item) => (e) => {
     if (!this.DetermineDisabled(item)) {
-      const { multi, showAll, items } = this.props;
+      const {multi, showAll, items, userCtrlKey} = this.props;
       let checkedValue = this.state.value;
-      if (multi) {
+      if ((userCtrlKey && multi && e.ctrlKey) || (multi && !userCtrlKey)) {
         // 可多选
-        checkedValue = checkedValue.filter((c) => c !== "ALL");
-        if (item.key === "ALL") {
-          checkedValue = ["ALL"];
+        checkedValue = checkedValue.filter((c) => c !== 'ALL');
+        if (item.key === 'ALL') {
+          checkedValue = ['ALL'];
         } else {
           if (checkedValue.indexOf(item.key) > -1) {
             checkedValue = checkedValue.filter((c) => c !== item.key);
-            checkedValue = Array.isArray(checkedValue)
-              ? checkedValue
-              : [checkedValue];
+            checkedValue = Array.isArray(checkedValue) ? checkedValue : [checkedValue];
           } else {
             checkedValue.push(item.key);
           }
         }
 
         if (showAll && items.length === checkedValue.length) {
-          checkedValue = ["ALL"];
+          checkedValue = ['ALL'];
         }
+      } else if (multi && !userCtrlKey) {
+        checkedValue = [item.key];
       } else {
         checkedValue = [item.key];
       }
 
-      this.setState({ value: [...checkedValue] }, () => {
-        const { onChange } = this.props;
-        typeof onChange === "function" &&
-          onChange(multi ? checkedValue : checkedValue[0]);
+      this.setState({value: [...checkedValue]}, () => {
+        const {onChange} = this.props;
+        typeof onChange === 'function' && onChange(multi ? checkedValue : checkedValue[0]);
       });
     }
   };
 
   renderTabItem = (item) => {
-    let { disabledTip, itemWidth } = this.props;
+    let {disabledTip, itemWidth} = this.props;
     disabledTip = disabledTip ? disabledTip : {};
-    let isDisabled = this.DetermineDisabled(item) ? "ss-tab-item-disabled" : "";
-    const isChecked = this.DetermineChecked(item) ? "ss-tab-item-checked" : "";
-    isDisabled = disabledTip[item.key]
-      ? isDisabled
-      : `${isDisabled ? "ss-tab-item-disabled-pure" : ""}`;
+    let isDisabled = this.DetermineDisabled(item) ? 'ss-tab-item-disabled' : '';
+    const isChecked = this.DetermineChecked(item) ? 'ss-tab-item-checked' : '';
+    isDisabled = disabledTip[item.key] ? isDisabled : `${isDisabled ? 'ss-tab-item-disabled-pure' : ''}`;
     return (
       <label
         key={item.key}
-        className={`${isDisabled} ${isChecked} ${
-          isDisabled && disabledTip[item.key] ? "tooltip" : ""
-        }`}
-        style={{ width: itemWidth ? itemWidth : "" }}
-        data-msg={
-          isDisabled && disabledTip[item.key] ? disabledTip[item.key] : null
-        }
+        className={`${isDisabled} ${isChecked} ${isDisabled && disabledTip[item.key] ? 'tooltip' : ''}`}
+        style={{width: itemWidth ? itemWidth : ''}}
+        data-msg={isDisabled && disabledTip[item.key] ? disabledTip[item.key] : null}
         onClick={this.handleClick(item)}
       >
         {item.value}
@@ -128,22 +117,15 @@ class Tabs extends React.PureComponent {
   };
 
   render() {
-    const { size, showAll, showAllText, items } = this.props;
+    const {size, showAll, showAllText, items} = this.props;
 
     let options = items || [];
 
     if (showAll) {
-      options = [
-        { key: "ALL", value: showAllText ? showAllText : "全部" },
-        ...items
-      ];
+      options = [{key: 'ALL', value: showAllText ? showAllText : '全部'}, ...items];
     }
 
-    return (
-      <div className={`ss-tab ss-tab-${size ? size : "normal"}`}>
-        {options.map(this.renderTabItem)}
-      </div>
-    );
+    return <div className={`ss-tab ss-tab-${size ? size : 'normal'}`}>{options.map(this.renderTabItem)}</div>;
   }
 }
 
--- src/demo/Tabs/index.js
+++ src/demo/Tabs/index.js
@@ -1,45 +1,43 @@
-import React from "react";
-import Tabs from "../../components/Tabs";
-import "./style.less";
+import React from 'react';
+import Tabs from '../../components/Tabs';
+import './style.less';
 
 class DemoTabs extends React.PureComponent {
+  onChange = (value) => {
+    console.log(value);
+  };
+
   render() {
     return (
-      <div className="demo-tabs">
-        <li key="1">
+      <div className='demo-tabs'>
+        <li key='1'>
           <span>normal</span>
-          <Tabs items={items} value={"key1"} />
+          <Tabs items={items} value={'key1'} onChange={this.onChange} />
         </li>
-        <li key="2">
+        <li key='2'>
           <span>big</span>
-          <Tabs items={items} value={"key1"} size="big" itemWidth={90} />
+          <Tabs items={items} value={'key1'} size='big' itemWidth={90} />
         </li>
-        <li key="3">
+        <li key='3'>
           <span>可多选</span>
-          <Tabs items={items} value={["key1", "key2"]} multi />
+          <Tabs items={items} value={['key1', 'key2']} multi />
         </li>
-        <li key="4">
+        <li key='4'>
           <span>显示全部</span>
-          <Tabs items={items} value={"key1"} showAll showAllText="ALL_KEYS" />
+          <Tabs items={items} value={'key1'} showAll showAllText='ALL_KEYS' />
         </li>
-        <li key="5">
+        <li key='5'>
           <span>全部+多选</span>
-          <Tabs
-            items={items}
-            value={["key1", "key2"]}
-            multi
-            showAll
-            showAllText="ALL_KEYS"
-          />
+          <Tabs items={items} value={['key1', 'key2']} multi showAll showAllText='ALL_KEYS' />
         </li>
-        <li key="6">
+        <li key='6'>
           <span>禁用元素</span>
-          <Tabs
-            items={items}
-            value={"key1"}
-            disabledItem={["key4", "key5"]}
-            disabledTip={Disabled_Tip}
-          />
+          <Tabs items={items} value={'key1'} disabledItem={['key4', 'key5']} disabledTip={Disabled_Tip} />
+        </li>
+
+        <li key='7'>
+          <span>使用ctrl 多选</span>
+          <Tabs items={items} value={'key1'} userCtrlKey multi onChange={this.onChange} />
         </li>
       </div>
     );
@@ -49,14 +47,14 @@ class DemoTabs extends React.PureComponent {
 export default DemoTabs;
 
 const Disabled_Tip = {
-  key4: "key4",
-  key5: "key5"
+  key4: 'key4',
+  key5: 'key5'
 };
 
 const items = [
-  { key: "key1", value: "key1" },
-  { key: "key2", value: "key2" },
-  { key: "key3", value: "key3" },
-  { key: "key4", value: "key4" },
-  { key: "key5", value: "key5" }
+  {key: 'key1', value: 'key1'},
+  {key: 'key2', value: 'key2'},
+  {key: 'key3', value: 'key3'},
+  {key: 'key4', value: 'key4'},
+  {key: 'key5', value: 'key5'}
 ];
